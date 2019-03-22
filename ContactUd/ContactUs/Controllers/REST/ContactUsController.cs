32409f35d086a8d669695cb682fb56356da2c964
LuisRosasC <Lrosasc@ucentral.edu.co>
2019-03-22 17:20:46 -0500

    prueba

---
+++ ContactUd/ContactUs/Controllers/REST/ContactUsController.cs
@@ -0,0 +1,44 @@
+﻿using ContactUs.DAO;
+using System.Web.Http;
+
+namespace ContactUs.Controllers.REST
+{
+    [RoutePrefix("api/contactUs")]
+    public class ContactUsController : ApiController
+    {
+        #region Properties
+
+        private readonly ContactUsDAO _dao;
+
+        #endregion Properties
+        
+        #region Constructor
+
+        public ContactUsController() =>
+            _dao = new ContactUsDAO();
+
+        #endregion Constructor
+
+        #region Public Methods
+
+        [HttpGet]
+        [Route("identificationType")]
+        public IHttpActionResult GetAllIdentificationTypes()
+        {
+            var identificationTypes = _dao.GetAllIdentificationTypes();
+            return Ok(identificationTypes);
+        }
+
+        [HttpPost]
+        [Route("contactUs")]
+        public IHttpActionResult UpdateContactUs([FromBody]Models.ContactUs content)
+        {
+            if (content == null)
+                return BadRequest("The parameters are missing.");
+            _dao.InsertContactUd(content);
+            return Ok();
+        }
+        
+        #endregion Public Methods
+    }
+}
